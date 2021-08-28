64f93723d3986791ef48cb9604dd9528ad7e48b0
brunoascenc <brunoascenc1@outlook.com>
2021-08-28 13:10:42 -0300

    menu data

---
+++ backend/data.js
@@ -0,0 +1,75 @@
+const data = {
+  menu: [
+    {
+      id: 1,
+      sabor: 'Calabresa',
+      descricao: 'Muussarela, calabresa e cebola',
+      preco: 30.0,
+    },
+    {
+      id: 2,
+      sabor: 'Mussarela',
+      descricao: 'Muussarela e Tomate',
+      preco: 30.0,
+    },
+    {
+      id: 3,
+      sabor: 'Baiana',
+      descricao:
+        'Calabresa moída, ovos, pimenta, cebola e um toque de parmesão',
+      preco: 35.0,
+    },
+    {
+      id: 4,
+      sabor: 'Frango Catupiry',
+      descricao: 'Frango e Catupiry',
+      preco: 35.0,
+    },
+
+    {
+      id: 5,
+      sabor: 'Quatro Queijos',
+      descricao: 'Frango e Catupiry',
+      preco: 40.0,
+    },
+    {
+      id: 6,
+      sabor: 'Quatro Queijos',
+      descricao: 'Mussarela, catupiry, provolone e parmesão',
+      preco: 42.0,
+    },
+    {
+      id: 7,
+      sabor: 'Atum',
+      descricao: 'Atum sólido e cebola',
+      preco: 38.0,
+    },
+    {
+      id: 8,
+      sabor: 'Caipira',
+      descricao: 'Frango, milho e catupiry',
+      preco: 42.0,
+    },
+    {
+      id: 9,
+      sabor: 'Brócolis',
+      descricao:
+        'Brócolis, bacon crocante e uma leve camada de mussarela ou catupiry.',
+      preco: 45.0,
+    },
+    {
+      id: 10,
+      sabor: 'Marguerita',
+      descricao: 'Mussarela com manjericão',
+      preco: 40.0,
+    },
+    {
+      id: 11,
+      sabor: 'Napolitana',
+      descricao: 'Mussarela, fatias de tomate e parmesão',
+      preco: 40.0,
+    },
+  ],
+};
+
+export default data;
