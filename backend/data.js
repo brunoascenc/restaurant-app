a73965c4023ae2642a8246f7f937f80d6818a86f
brunoascenc <brunoascenc1@outlook.com>
2021-08-28 17:16:13 -0300

    api quantity reference

--- backend/data.js
+++ backend/data.js
@@ -3,18 +3,21 @@ const data = {
     {
       id: 1,
       sabor: 'Calabresa',
+      quantity: 1,
       descricao: 'Muussarela, calabresa e cebola',
       preco: 30.0,
     },
     {
       id: 2,
       sabor: 'Mussarela',
+      quantity: 1,
       descricao: 'Muussarela e Tomate',
       preco: 30.0,
     },
     {
       id: 3,
       sabor: 'Baiana',
+      quantity: 1,
       descricao:
         'Calabresa moída, ovos, pimenta, cebola e um toque de parmesão',
       preco: 35.0,
@@ -22,6 +25,7 @@ const data = {
     {
       id: 4,
       sabor: 'Frango Catupiry',
+      quantity: 1,
       descricao: 'Frango e Catupiry',
       preco: 35.0,
     },
@@ -29,30 +33,35 @@ const data = {
     {
       id: 5,
       sabor: 'Quatro Queijos',
+      quantity: 1,
       descricao: 'Frango e Catupiry',
       preco: 40.0,
     },
     {
       id: 6,
       sabor: 'Quatro Queijos',
+      quantity: 1,
       descricao: 'Mussarela, catupiry, provolone e parmesão',
       preco: 42.0,
     },
     {
       id: 7,
       sabor: 'Atum',
+      quantity: 1,
       descricao: 'Atum sólido e cebola',
       preco: 38.0,
     },
     {
       id: 8,
       sabor: 'Caipira',
+      quantity: 1,
       descricao: 'Frango, milho e catupiry',
       preco: 42.0,
     },
     {
       id: 9,
       sabor: 'Brócolis',
+      quantity: 1,
       descricao:
         'Brócolis, bacon crocante e uma leve camada de mussarela ou catupiry.',
       preco: 45.0,
@@ -60,12 +69,14 @@ const data = {
     {
       id: 10,
       sabor: 'Marguerita',
+      quantity: 1,
       descricao: 'Mussarela com manjericão',
       preco: 40.0,
     },
     {
       id: 11,
       sabor: 'Napolitana',
+      quantity: 1,
       descricao: 'Mussarela, fatias de tomate e parmesão',
       preco: 40.0,
     },
