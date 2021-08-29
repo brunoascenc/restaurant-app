3f2a2d6dac0334a04d881389acaed87482b51d1d
brunoascenc <brunoascenc1@outlook.com>
2021-08-29 13:35:38 -0300

    kitchen redux

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -2,17 +2,19 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import menuReducer from './menu-data/menu-reducer';
 import checkoutReducer from './checkout-list/checkout-reducer';
+import kitchenReducer from './kitchen/kitchen-reducer';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['checkout'],
+  whitelist: ['checkout', 'orders'],
 };
 
 const rootReducer = combineReducers({
   menu: menuReducer,
   checkout: checkoutReducer,
+  orders: kitchenReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
