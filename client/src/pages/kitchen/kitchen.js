9e446d31dd81dc4bb4c5097bbdd016670eae238e
brunoascenc <brunoascenc1@outlook.com>
2021-08-29 13:35:49 -0300

    kitchen page

--- client/src/pages/kitchen/kitchen.js
+++ client/src/pages/kitchen/kitchen.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const kitchen = () => {
-  return <div></div>;
+const Kitchen = () => {
+  // const dispatch = useDispatch();
+  const orders = useSelector((state) => state.orders.results);
+
+  return (
+    <div>
+      <h1>Pedidos Pendentes</h1>
+      <ul>
+        {orders.map((item) => {
+          return (
+            <li key={item.id}>
+              <div>
+                {item.sabor}
+                {item.descricao}
+              </div>
+              <span>Preço: R$: </span>
+              {item.preco}
+              <h1>{item.quantity}</h1>
+              <button>Finalizado</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
-export default kitchen;
+export default Kitchen;
